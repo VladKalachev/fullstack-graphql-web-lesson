a8f8b95a894fca6844ab6e9fc13ffed2cf1dbcbc
Corvax <vlvete1990@yandex.ru>
2021-06-08 18:53:00 +0300

    chore: added component NavBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Box, Link, Flex, Button, Heading } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const router = useRouter();
+  let body = null;
+
+  if (true) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>register</Link>
+        </NextLink>
+      </>
+    );
+    // user is logged in
+  } else {
+    body = (
+      <Flex align="center">
+        <NextLink href="/create-post">
+          <Button as={Link} mr={4}>
+            create post
+          </Button>
+        </NextLink>
+        <Box mr={2}>{"username"}</Box>
+        <Button
+          variant="link"
+        >
+          logout
+        </Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex zIndex={1} position="sticky" top={0} bg="tan" p={4}>
+      <Flex flex={1} m="auto" align="center" maxW={800}>
+        <NextLink href="/">
+          <Link>
+            <Heading>LiReddit</Heading>
+          </Link>
+        </NextLink>
+        <Box ml={"auto"}>{body}</Box>
+      </Flex>
+    </Flex>
+  );
+};
