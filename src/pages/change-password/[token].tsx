a0bc1d99fbf53c3e2a6b8282a7207010688ee37c
Corvax <vlvete1990@yandex.ru>
2021-06-19 22:31:17 +0300

    chore: added textSnippet

--- src/pages/change-password/[token].tsx
+++ src/pages/change-password/[token].tsx
@@ -11,10 +11,10 @@ import { useChangePasswordMutation } from "../../generated/graphql";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../../utils/createUrqlClient";
 
-const ChangePassword: NextPage<{token: string}> = ({ token }) => {
+const ChangePassword: NextPage = () => {
   const router = useRouter();
+  const { token } = router.query;
   const [, changePassword] = useChangePasswordMutation();
-
   const [tokenError, setTokenError] = useState("");
   
   return (
@@ -24,7 +24,7 @@ const ChangePassword: NextPage<{token: string}> = ({ token }) => {
         onSubmit={async (values, { setErrors }) => {
           const response = await changePassword({
             newPassword: values.newPassword,
-            token,
+            token: typeof token === "string" ? token : "",
           })
           if (response.data?.changePassword.errors) {
             const errorMap = toErrorMap(response.data.changePassword.errors);
@@ -70,12 +70,5 @@ const ChangePassword: NextPage<{token: string}> = ({ token }) => {
     </Wrapper>
   );
 };
-
-ChangePassword.getInitialProps = ({ query }) => {
-    console.log('query', query)
-    return {
-        token: query.token as string
-    }
-}
 
 export default withUrqlClient(createUrqlClient)(ChangePassword);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
-import { NavBar } from '../components/NavBar';
+import { Stack } from '@chakra-ui/react';
 import { withUrqlClient } from 'next-urql';
 import NextLink from 'next/link';
 import { createUrqlClient } from '../utils/createUrqlClient';
 import { usePostsQuery } from '../generated/graphql';
-import { Link } from '@chakra-ui/react';
+import { Link, Box, Heading, Text } from '@chakra-ui/react';
 import { Layout } from '../components/Layout';
 
 const Index = () => {
-  const [{data}] = usePostsQuery()
+  const [{data}] = usePostsQuery({
+    variables: {
+      limit: 10
+    }
+  });
   return (
       <Layout>
         <NextLink href="/create-post">
@@ -17,7 +21,14 @@ const Index = () => {
         {!data ? ( 
           <div>loading...</div> 
         ) : (
-          data?.posts.map((p) => <div key={p.id}>{p.title}</div>)
+          <Stack spacing={8}>
+          {data?.posts.map((p) => (
+            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{p.title}</Heading>
+              <Text mt={4}>{p.textSnippet}</Text>
+            </Box>
+          ))}
+          </Stack>
         )}
       </Layout>
   )
