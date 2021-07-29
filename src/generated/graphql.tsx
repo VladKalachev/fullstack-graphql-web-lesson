3e179c895bf32ea048c95ac0a8f9c1c27062a588
Corvax <vlvete1990@yandex.ru>
2021-07-29 22:02:30 +0300

    chore: added creator username

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -22,6 +22,7 @@ export type FieldError = {
 
 export type Mutation = {
   __typename?: 'Mutation';
+  vote: Scalars['Boolean'];
   createPost: Post;
   updatePost?: Maybe<Post>;
   deletePost: Scalars['Boolean'];
@@ -31,6 +32,12 @@ export type Mutation = {
   login: UserResponse;
   deleteUser: Scalars['Boolean'];
   logout: Scalars['Boolean'];
+};
+
+
+export type MutationVoteArgs = {
+  value: Scalars['Int'];
+  postId: Scalars['Int'];
 };
 
 
@@ -89,6 +96,7 @@ export type Post = {
   text: Scalars['String'];
   points: Scalars['Float'];
   creatorId: Scalars['Float'];
+  creator: User;
   createdAt: Scalars['String'];
   updatedAt: Scalars['String'];
   textSnippet: Scalars['String'];
@@ -264,6 +272,10 @@ export type PostsQuery = (
     & { posts: Array<(
       { __typename?: 'Post' }
       & Pick<Post, 'id' | 'createdAt' | 'updatedAt' | 'title' | 'textSnippet'>
+      & { creator: (
+        { __typename?: 'User' }
+        & Pick<User, 'id' | 'username'>
+      ) }
     )> }
   ) }
 );
@@ -380,6 +392,10 @@ export const PostsDocument = gql`
       updatedAt
       title
       textSnippet
+      creator {
+        id
+        username
+      }
     }
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,10 @@ import { Link, Box, Heading, Text, Flex } from '@chakra-ui/react';
 import { Layout } from '../components/Layout';
 
 const Index = () => {
-  const [variables, setVariables] = useState({limit: 10, cursor: null as null | string});
+  const [variables, setVariables] = useState({
+    limit: 15, 
+    cursor: null as null | string
+  });
   const [{data, fetching}] = usePostsQuery({
     variables,
   });
@@ -29,7 +32,7 @@ const Index = () => {
           <Stack spacing={8}>
             {data?.posts.posts.map((p) => (
               <Box key={p.id} p={5} shadow="md" borderWidth="1px">
-                <Heading fontSize="xl">{p.title}</Heading>
+                <Heading fontSize="xl">{p.title}</Heading> {p.creator.username}
                 <Text mt={4}>{p.textSnippet}</Text>
               </Box>
             ))}
