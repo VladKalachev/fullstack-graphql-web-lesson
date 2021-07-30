8e3d64b4bd4390704b6843aa0f2596f9806ca304
Corvax <vlvete1990@yandex.ru>
2021-07-30 22:50:50 +0300

    chore: added vote

--- src/components/UpdootSection.tsx
+++ src/components/UpdootSection.tsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { Flex, IconButton } from "@chakra-ui/react";
 import {
   PostSnippetFragment,
-  // useVoteMutation,
-  // VoteMutation,
+  useVoteMutation,
+  VoteMutation,
 } from "../generated/graphql";
 import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 // import gql from "graphql-tag";
@@ -56,45 +56,40 @@ export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
   const [loadingState, setLoadingState] = useState<
     "updoot-loading" | "downdoot-loading" | "not-loading"
   >("not-loading");
-  const [vote] = useVoteMutation();
+  const [, vote] = useVoteMutation();
   return (
     <Flex direction="column" justifyContent="center" alignItems="center" mr={4}>
       <IconButton 
         onClick={async () => {
-          if (post.voteStatus === 1) {
-            return;
-          }
+          // if (post.voteStatus === 1) {
+          //   return;
+          // }
           setLoadingState("updoot-loading");
           await vote({
-            variables: {
               postId: post.id,
               value: 1,
             },
-            update: false //(cache) => updateAfterVote(1, post.id, cache),
-          });
+          );
           setLoadingState("not-loading");
         }}
-        variantColor={post.voteStatus === 1 ? "green" : undefined}
+        // variantColor={post.voteStatus === 1 ? "green" : undefined}
         isLoading={loadingState === "updoot-loading"}
         aria-label="updoot post"
         icon={<ChevronUpIcon size="24px" />}
       />
       {post.points}
       <IconButton  onClick={async () => {
-          if (post.voteStatus === -1) {
-            return;
-          }
+          // if (post.voteStatus === -1) {
+          //   return;
+          // }
           setLoadingState("downdoot-loading");
           await vote({
-            variables: {
               postId: post.id,
               value: -1,
-            },
-            update: false //(cache) => updateAfterVote(-1, post.id, cache),
-          });
+            });
           setLoadingState("not-loading");
         }}
-        variantColor={post.voteStatus === -1 ? "red" : undefined}
+        // variantColor={post.voteStatus === -1 ? "red" : undefined}
         isLoading={loadingState === "downdoot-loading"}
         aria-label="downdoot post"
         icon={<ChevronDownIcon size="24px"
